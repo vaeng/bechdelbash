368ab5b96ca07306e34e72a190d4bc7b574b43f3
Christian Christian <christiaaan@gmail.com>
2018-11-19 20:42:53 +0000

    preparation for category search querries

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 import os
 import re
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, Response
 import psycopg2
 from psycopg2 import sql
+import json
 
 
 # Set up database
-# DATABASE_URL = "sqlite:///movies.db"
 DATABASE_URL = os.environ['DATABASE_URL']
 connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = connection.cursor()
@@ -35,6 +35,50 @@ def bechdelresults(category, filters):
     result = cursor.fetchone()
     return ("The average bechdel score for movies with " + filters + " in "  + category + " is " + str(result[0]/result[1]) + " with " + str(result[0]) + " movies.")
 
+@app.route("/decades", methods=["GET"])
+def decades():
+    """Returns development of bechdel score over the decades for search term and category"""
+
+    decade_result = [{
+		"category" : "",
+		"filterterm" : "",
+		"totalmovies" : 0,
+		"totalaverage" : 0,
+	   }]
+
+    # Ensure category was submitted
+    category = request.args.get("category")
+    print("category: " + category)
+    if not category:
+        return "must provide category"
+
+    # Ensure filterterm was submitted
+    filterterm = request.args.get("filterterm")
+    if not filterterm:
+        return "must provide filterterm"
+    # setting up dict object
+    decade_result[0]["category"] = category
+    decade_result[0]["filterterm"] = filterterm
+
+    filterstring = "%" + filterterm + "%"
+
+    try:
+        cursor.execute(sql.SQL("SELECT SUM(rating), COUNT(rating) FROM list WHERE {} ILIKE %s").format(sql.Identifier(category)), (filterstring,))
+    except:
+        return Response(json.dumps(decade_result),  mimetype='application/json')
+
+    (ratingsum, moviesum) = cursor.fetchone()
+
+    if int(moviesum) >0:
+        decade_result[0]["totalmovies"] = moviesum
+        decade_result[0]["totalaverage"] = "{:.10}".format(ratingsum/moviesum)
+
+    # return the desired json obect
+    return Response(json.dumps(decade_result),  mimetype='application/json')
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
